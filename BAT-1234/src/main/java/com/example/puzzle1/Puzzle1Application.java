e6914425ad6054774059d3a1d4fefb4e6c5d2a0f
Michal Machowski <michal.machowski@auto1.com>
2018-11-15 16:50:32 +0100

    sample code

---
+++ BAT-1234/src/main/java/com/example/puzzle1/Puzzle1Application.java
@@ -0,0 +1,12 @@
+package com.example.puzzle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Puzzle1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Puzzle1Application.class, args);
+	}
+}
