d04ed9e9f5c01abfce6d64c9adb535f220cdd7ac
Michal Machowski <michal.machowski@auto1.com>
2018-11-15 16:57:53 +0100

    BAT-12345 please review it

---
+++ BAT-12345/src/main/java/com/piksel/rm/service/PaymentService.java
@@ -0,0 +1,33 @@
+package com.piksel.rm.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.piksel.rm.repository.StudioRepository;
+import com.piksel.rm.service.dto.PaymentDTO;
+
+@Service
+public class PaymentService {
+
+	private final Logger log = LoggerFactory.getLogger(PaymentService.class);
+
+	@Autowired
+	private StudioRepository studioRepository;
+
+	public List<PaymentDTO> findAll(Pageable page) {
+		log.debug("Request to get all Payments");
+		return studioRepository.calculateAllPayments(page);
+	}
+
+	public Optional<PaymentDTO> find(String id) {
+		log.debug("Request to get Payment : {}", id);
+		return studioRepository.calculatePayment(id);
+	}
+
+}
